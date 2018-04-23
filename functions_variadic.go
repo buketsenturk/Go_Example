e0d335ada7fdb5ed852ce94e957fb531b66d9dfa
buketsenturk96 <38646242+buketsenturk96@users.noreply.github.com>
2018-04-23 14:28:53 +0300

    Update and rename Functions.go to functions_variadic.go

---
+++ functions_variadic.go
@@ -0,0 +1,58 @@
+// $go run variadic-functions.go
+
+package main
+import "fmt"
+
+
+func plus(a int, b int) int {
+    return a + b
+}
+
+func plusPlus(a, b, c int) int {
+    return a + b + c
+}
+
+func vals() (int, int) {
+    return 3, 7
+}
+
+
+func sum(nums ...int) {
+    fmt.Print(nums, " ")
+    total := 0
+    for _, num := range nums {
+        total += num
+    }
+    fmt.Println(total)
+}
+
+func main() {
+
+
+    res := plus(1, 2)
+    fmt.Println("1+2 =", res)
+    res = plusPlus(1, 2, 3)
+    fmt.Println("1+2+3 =", res)
+    
+    
+    // multiple return
+    
+    a, b := vals()
+    fmt.Println(a)      // 3
+    fmt.Println(b)      // 7
+    
+    _, c := vals()
+    fmt.Println(c)         // 7 
+    
+    
+    //////-------------- variadic --------------------------
+    
+    sum(1, 2)
+    sum(1, 2, 3)
+    
+    nums := []int{1, 2, 3, 4}
+    sum(nums...)
+    
+    
+    
+}
