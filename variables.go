0efa0b67b023c9ca8218e1d79f68f29a52ccc07a
buketsenturk96 <38646242+buketsenturk96@users.noreply.github.com>
2018-04-23 11:48:31 +0300

    Create variables.go
    $ go run variables.go
    
    string
    
    15,6
    x is of type float64
    
    true
    
    0
    
    
    string2

---
+++ variables.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+func main() {
+    
+    var a = "string"
+    fmt.Println(a)
+    
+    
+   var x float64
+   x = 15.6
+   fmt.Println(x)
+   fmt.Printf("x is of type %T\n", x)
+   
+   
+    var d = true
+    fmt.Println(d)
+    
+    var e int
+    fmt.Println(e)
+    
+    
+    f := "string2"
+    fmt.Println(f)
+
+    
+}
